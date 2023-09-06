12034b038122524aaf90ada34dc0df51236d1902
alynnesantos <alynnebianca774@gmail.com>
2023-09-06 13:50:55 -0300

    top of page home

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.home')
 
-@section('title', 'Vendas')
+@section('title', 'Murangotango | E-commerce')
 @section('content')
 @endsection
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -14,26 +14,71 @@
       <!-- CSS Bootstrap -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-      <!-- CSS da aplicação -->
-      <link rel="stylesheet" href="/css/styles.css">
+      <style>
+        body {
+            font-family: 'Inter', sans-serif;
+            font-weight: 400;
+            margin: 0;
+            color:#0F244A;
+        }
+
+        footer {
+          background-color: #4169E1;
+          color: #FFFFFF;
+          padding: 45px 0;
+          text-align: center;
+          font-size: 1.2rem;
+
+        }
+
+        #nav {
+          background-color: #4169E1;
+        }
+
+        #nav .navbar-nav .nav-link {            
+            color: #FFFFFF;
+        }
+      </style>
+
   </head>
     <body>
-      <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
-        <a href="/" class="navbar-brand"> MyShop </a>
+      <nav class="navbar navbar-dark navbar-expand-md bg-sm pl-5 pr-5 mb-5 px-4 p-4" id="nav">
+        <a href="#" class="btn btn-sm">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M4 6l16 0"></path>
+            <path d="M4 12l16 0"></path>
+            <path d="M4 18l16 0"></path>
+         </svg>
+        </a>
+        <div class="container text-center">
+        <a href="/" class="navbar-brand"> MurangotangoShop </a>
           <div class="collapse navbar-collapse">
-            <div class="navbar-nav">
+            <div class="navbar-nav fw-bold text-white">
               <a href="/" class="nav-link">Home</a>
               <a href="/" class="nav-link">Categorias</a> 
               <a href="/" class="nav-link">Cadastrar</a>
             </div>
           </div>
-          <a href="#" class="btn btn-sm"></a><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-            <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-            <path d="M17 17h-11v-14h-2"></path>
-            <path d="M6 5l14 1l-1 7h-13"></path>
-         </svg></a>
+          <div class="container text-center">
+          <div class="row align-self-center">
+            <div class="col-md-8">
+              <input class="form-control form-control-lg"
+                  type="text" role= "search"
+                  placeholder="Busque aqui">
+            </div>
+          </div>
+        </div>
+          <a href="#" class="btn btn-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+              <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+              <path d="M17 17h-11v-14h-2"></path>
+              <path d="M6 5l14 1l-1 7h-13"></path>
+            </svg>
+          </a>
+        </div>
       </nav>
       
       @yield('content')
